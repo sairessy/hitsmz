40ba7be212ef87cf13465c24c0f16a0e5fa94a1c
sairessy <sairessy@gmail.com>
2020-12-10 05:21:52 -0800

    m

---
+++ public/js/Data.js
@@ -0,0 +1,179 @@
+class Data {
+    constructor() {
+        this.data = this.getData();
+    }
+
+    getData() {
+        return (
+            [
+                {
+                    "id": 0,
+                    "hit": {
+                        "file": "",
+                        "title": "Confissão"
+                    },
+    
+                    "author": {
+                        "name": "azagaia",
+                        "others": ["Lena Baule"],
+                        "photo": ""
+                    }
+                },
+    
+                {
+                    "id": 1,
+                    "hit": {
+                        "file": "",
+                        "title": "Cu gordo"
+                    },
+    
+                    "author": {
+                        "name": "Duas carras",
+                        "others": [],
+                        "photo": "1"
+                    }
+                },
+    
+                {
+                    "id": 2,
+                    "hit": {
+                        "file": "",
+                        "title": "Não sei o que fazer"
+                    },
+    
+                    "author": {
+                        "name": "Fiex",
+                        "others": ["Hernani"],
+                        "photo": ""
+                    }
+                },
+    
+                {
+                    "id": 3,
+                    "hit": {
+                        "file": "",
+                        "title": "Anti chula"
+                    },
+    
+                    "author": {
+                        "name": "G2",
+                        "others": ["Trez Agah"],
+                        "photo": ""
+                    }
+                },
+
+                {
+                    "id": 4,
+                    "hit": {
+                        "file": "",
+                        "title": "Quero uma friend"
+                    },
+    
+                    "author": {
+                        "name": "Gpro",
+                        "others": ["G2, Trez Agah"],
+                        "photo": ""
+                    }
+                },
+    
+                {
+                    "id": 5,
+                    "hit": {
+                        "file": "",
+                        "title": "4 estações do amor"
+                    },
+    
+                    "author": {
+                        "name": "Ivete",
+                        "others": [],
+                        "photo": "1"
+                    }
+                },
+    
+                {
+                    "id": 6,
+                    "hit": {
+                        "file": "",
+                        "title": "Por causa de homem"
+                    },
+    
+                    "author": {
+                        "name": "Mimae",
+                        "others": [],
+                        "photo": ""
+                    }
+                },
+    
+                {
+                    "id": 7,
+                    "hit": {
+                        "file": "",
+                        "title": "Minha mãe"
+                    },
+    
+                    "author": {
+                        "name": "Moz Pipa",
+                        "others": [],
+                        "photo": ""
+                    }
+                },
+
+                {
+                    "id": 8,
+                    "hit": {
+                        "file": "",
+                        "title": "Lirandzo"
+                    },
+    
+                    "author": {
+                        "name": "Neyma",
+                        "others": [],
+                        "photo": ""
+                    }
+                },
+    
+                {
+                    "id": 9,
+                    "hit": {
+                        "file": "",
+                        "title": "Himaka Ya Mili"
+                    },
+    
+                    "author": {
+                        "name": "Neyma",
+                        "others": [],
+                        "photo": ""
+                    }
+                },
+    
+                {
+                    "id": 10,
+                    "hit": {
+                        "file": "",
+                        "title": "Tu és tão linda"
+                    },
+    
+                    "author": {
+                        "name": "Trio Fam",
+                        "others": [],
+                        "photo": ""
+                    }
+                },
+    
+                {
+                    "id": 11,
+                    "hit": {
+                        "file": "",
+                        "title": "Nascer de novo"
+                    },
+    
+                    "author": {
+                        "name": "Valdemiro José",
+                        "others": [],
+                        "photo": ""
+                    }
+                }
+            ]
+        );
+    }
+}
